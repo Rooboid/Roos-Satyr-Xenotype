7464e63e096c08d568810b3bf444562eaa19732f
Zelanious <71904533+Zelanious@users.noreply.github.com>
2023-05-04 01:20:39 +0100

    removed anima rod

--- Source/Roos_Satyr_Xenotype/PawnFur.cs
+++ Source/Roos_Satyr_Xenotype/PawnFur.cs
@@ -1,13 +1,6 @@
 ﻿using HarmonyLib;
-using RimWorld;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using Verse;
-using Verse.Noise;
 
 namespace Roos_Satyr_Xenotype
 {
