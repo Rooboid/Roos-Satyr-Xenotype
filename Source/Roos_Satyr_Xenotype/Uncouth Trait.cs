13f772e0c8e20c2af0a617b1ba806bca99e7ff43
Zelanious <71904533+Zelanious@users.noreply.github.com>
2023-05-08 18:13:37 +0100

    attempted fix for pawnbeauty not working

--- Source/Roos_Satyr_Xenotype/Uncouth Trait.cs
+++ Source/Roos_Satyr_Xenotype/Uncouth Trait.cs
@@ -60,15 +60,18 @@ namespace Roos_Satyr_Xenotype
                 if (pawn == PawnAsker)
                     continue;
 
-                float beautyAmount = pawn.GetStatValue(StatDefOf.Beauty);
+                float beautyAmount = pawn.GetStatValue(StatDefOf.PawnBeauty);
 
                 if (beautyAmount > 2.0f)
                     beautyAmount = 2.0f;
 
                 if (beautyAmount > 0.0f)
+                    Log.Message("found beautiful pawn " + pawn.Name + "with a beauty value of: " + pawn.GetStatValue(StatDefOf.PawnBeauty) + ".");
                     TotalBeauty += beautyAmount;
                 
             }
+            Log.Message("Total beauty was" + TotalBeauty+ ".");
+            Log.Message("");
 
             //Log.Message("Checked beauty for uncouth pawn " + PawnAsker.Name + " total beauty was " + TotalBeauty);
             if (TotalBeauty < 1f)
