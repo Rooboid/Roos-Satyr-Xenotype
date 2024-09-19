d1893a5038659467b7088e536c66775b117f6577
Zelanious <71904533+Zelanious@users.noreply.github.com>
2024-09-19 19:27:09 +0100

    minor patch to pawn render

--- Source/Roos_Satyr_Xenotype/PawnFur.cs
+++ Source/Roos_Satyr_Xenotype/PawnFur.cs
@@ -11,13 +11,14 @@ namespace Roos_Satyr_Xenotype
     {
         static bool Prefix(Pawn pawn, ref Graphic __result)
         {
+            if (!pawn.RaceProps.Humanlike) {
+                return true;
+            }
+
             if (pawn.genes.HasActiveGene(RBSF_DefOf.RBM_UnguligradeLegs))
             {
-                //Mesh mesh = HumanlikeMeshPoolUtility.GetHumanlikeBodySetForPawn(___pawn).MeshAt(facing);
                 Graphic graphic = GraphicDatabase.Get<Graphic_Multi>(pawn.story.furDef.GetFurBodyGraphicPath(pawn), ShaderDatabase.CutoutSkinOverlay, Vector2.one, pawn.story.HairColor, Color.white, null, pawn.story.bodyType.bodyNakedGraphicPath);
                 __result = graphic;
-                //Material mat2 = graphic.MatAt(facing, null);
-                //GenDraw.DrawMeshNowOrLater(mesh, shellLoc, quat, mat2, flags.FlagSet(PawnRenderFlags.DrawNow));
                 return false;
             }
             return true;
